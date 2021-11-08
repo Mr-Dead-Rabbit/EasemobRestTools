c4d065a59cb0eba936e6ae315629605183534a99
chaojie.yuan <yuanchaojiedev@163.com>
2021-11-08 17:05:11 +0800

    修改

--- get_user.sh
+++ get_user.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
-if [[ -z $APPKEY ]]  
-then 
-read -p "请输入APPKEY,(如：easemob-demo#easeim, and press ENTER): " APPKEY
-fi
+APPKEY="用户自己的appkey"
+client_id="用户自己的client_id" 
+client_secret="用户自己的client_secret" 
 
+HOST="a1.easemob.com"
 org_name=`echo ${APPKEY%#*}`
 app_name=`echo ${APPKEY#*#}`
 
-read -p "请输入域名(如：a1.easemob.com , and press ENTER): " HOST 
-
-if [[ -z $HOST ]]; then
-HOST=a1.easemob.com
-fi
-
-read -p "请输入client_id： (and press enter): " client_id 
-read -p "请输入client_secret： (and press enter): " client_secret 
-
-curl -X POST -s \
+response=`curl -X POST -s \
 -H 'Content-Type: application/json' \
 -H 'Accept: application/json' \
 -d "{ 
    \"grant_type\": \"client_credentials\",  
    \"client_id\": \"$client_id\", 
    \"client_secret\": \"$client_secret\"  
- }" "http://$HOST/$org_name/$app_name/token"
+ }" "http://$HOST/$org_name/$app_name/token"`
+access_token=`echo $response | sed -n 's/.*access_token\":\"\([^"]*\)\".*/\1/p'`
+echo $access_token
 
-    
-echo $response
+read -p "请输入要查询的用户名： (and press enter): " username 
+
+user_response=`curl -X GET -s \
+-H "Accept:application/json" \
+-H "Authorization: Bearer $access_token" \
+"http://$HOST/$org_name/$app_name/users/$username"`
+
+echo $user_response
 
 
 
